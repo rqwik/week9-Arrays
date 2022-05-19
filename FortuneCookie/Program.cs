199f45fa803f487f6bf69ac001521284084157f2
marinakamardina <kamardina.mari@gmail.com>
2022-05-19 14:38:10 +0300

    changes

--- FortuneCookie/Program.cs
+++ FortuneCookie/Program.cs
@@ -16,19 +16,6 @@ namespace FortuneCookie
                 "See a unicorn in the street",
             };
             Console.WriteLine("Your cookie is: {0}", fortune.GetValue(new Random().Next(fortune.Length)));
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(); // OR OTHER VARIANT
-
-            Console.WriteLine("Fortune Cookie. Let's see what awaits you today");
-            string[] cookies = { "Win a Jackpot", "Fall in love", "Get a love letter", "Miss the bus", "See a unicorn in the street"};
-
-            Random rnd = new Random();
-            int randomIndex = rnd.Next(0, cookies.Length);
-
-            Console.WriteLine($"Your cookie is: {cookies[randomIndex]}");
-
-
         }
     }
 }
--- MatterOfTaste/Program.cs
+++ MatterOfTaste/Program.cs
@@ -25,23 +25,6 @@ namespace MatterOfTaste
             {
                 Console.WriteLine("Every man to his taste.");
             }
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(); // OR OTHER VARIANT
-
-            Console.WriteLine("Matter of taste)");
-            Console.Write("Hi, please enter your favorite color: ");
-            string[] color = { "red", "blue", "green" };
-            string userChoice = Console.ReadLine().ToLower();
-
-            if (Array.Exists(color, color => color.Equals(userChoice)))
-            {
-                Console.WriteLine("We are perfect together.");
-            }
-            else
-            {
-                Console.WriteLine("Every man to his taste.");
-            }
 
         }
     }
